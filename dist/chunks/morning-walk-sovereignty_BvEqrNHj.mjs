93fba63ec00e12bb6d82792b204253f78c3c9c13
agitlin <alex.gitlin@gmail.com>
2026-05-30 22:54:27 +0300

    chore: add .gitignore; stop tracking node_modules and dist

--- dist/chunks/morning-walk-sovereignty_BvEqrNHj.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./morning-walk-sovereignty_CIwiVFCK.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
